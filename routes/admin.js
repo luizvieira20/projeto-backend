3427e74911830c9a39f2dc3bd0e32d47cabfb202
Luiz Vieira <luizvieira740@gmail.com>
2023-12-04 21:44:23 -0300

    Feito rota de admin para alterar dados

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,7 @@ const UsuarioService = require('../Model/Usuarios');
 
 // Verificar Token
 
-const verificaTokenJWT = async(token) => {
+  const verificaTokenJWT = async(token) => {
     try{
       const verificado = jwt.verify(token, process.env.SECRET);
       const user = await UsuarioService.verificarEmail(verificado.email);
@@ -15,67 +15,112 @@ const verificaTokenJWT = async(token) => {
     }catch (error) {
       return null;
     }
-}
+  }
 
   // Deletar usuário
   
-  router.delete('/admin/:id', async (req, res) => {
+  router.delete('/admin/deletar/:id', async (req, res) => {
     const token = req.headers.authorization.split(' ')[1];
     const conta = await verificaTokenJWT(token);
     if(conta != null){
-        if(conta == true){
-          const userId = await UsuarioService.verificarId(req.params.id);
+      if(conta == true){
+        const userId = await UsuarioService.verificarId(req.params.id);
   
-          if(userId){
-            if(userId.privilegios == 0){
-              await userId.destroy();
-              res.json({msg: 'Usuário excluído com sucesso'});
-            }else{
-              res.status(403).json({msg: "Erro, impossível apagar outro usuário administrador"});
-            }
+        if(userId){
+          if(userId.privilegios == 0){
+            await userId.destroy();
+            res.json({msg: 'Usuário excluído com sucesso'});
           }else{
-            res.status(403).json({msg: "Erro, usuário inexistente!"});
+            res.status(403).json({msg: "Erro, impossível apagar outro usuário administrador"});
           }
         }else{
-          res.status(403).json({msg: "Erro, permissão negada!"});
+          res.status(403).json({msg: "Erro, usuário inexistente!"});
         }
-    }else {
+      }else{
+        res.status(403).json({msg: "Erro, permissão negada!"});
+      }
+    }else{
       res.status(403).json({msg: "Erro, token inválido!"});
     }
   });
 
-  //Criar outro administrador
+  // Criar outro administrador
   
   router.post('/admin/cadastro', async (req, res) => {
     try{
+      const token = req.headers.authorization.split(' ')[1];
+      const conta = await verificaTokenJWT(token);
+      const { usuario, email, senha } = req.body;
+
+      if(conta != null){
+        if(conta == true){
+          const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+          if (usuario.lenght === 0 || email.length === 0 || senha.length === 0){
+            res.json('Erro. Por favor, preencha todos os campos.');
+          }else if (emailRegex.test(email) === false) res.json('Erro. Digite um e-mail válido');
+
+          const existente = await UsuarioService.verificarEmail(email);
+            
+          if(existente){
+            res.status(403).json({msg: "Erro, email já cadastrado!"})
+          }else{
+            let user = await UsuarioService.novo(usuario, email, senha, 1);
+            res.json({mensagem: "Administrador cadastrado com sucesso!", user});
+          }
+        }else{
+          res.status(403).json({msg: "Erro, permissão negada!"});
+        }
+      }else{
+        res.status(403).json({msg: "Erro, token inválido!"});
+      }
+    }catch (error) {
+      res.status(400).json({mensagem: "Erro ao cadastrar administrador, faça login.", error});
+    }
+  });
+
+// Alteração de outros usuários
+
+router.put('/admin/alterar/:id', async (req, res) => {
+  try{
     const token = req.headers.authorization.split(' ')[1];
     const conta = await verificaTokenJWT(token);
     const { usuario, email, senha } = req.body;
 
-    if(conta != null){
-        if(conta == true){
-            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
-            if (usuario.lenght === 0 || email.length === 0 || senha.length === 0){
-                res.json('Erro. Por favor, preencha todos os campos.');
-            }else if (emailRegex.test(email) === false) res.json('Erro. Digite um e-mail válido');
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+    if(usuario.lenght === 0 || email.length === 0 || senha.length === 0){
+      res.json({msg: 'Erro. Por favor, preencha todos os campos.'});
+    }else if (emailRegex.test(email) === false) res.json({msg: 'Erro. Digite um e-mail válido'});
 
-            const existente = await UsuarioService.verificarEmail(email);
-            
-            if(existente){
-              res.status(403).json({msg: "Erro, email já cadastrado!"})
-            }else{
-              let user = await UsuarioService.novo(usuario, email, senha, 1);
-              res.json({mensagem: "Administrador cadastrado com sucesso!", user});
-            }
+    if(conta != null){
+      if(conta == true){
+        const userId = await UsuarioService.verificarId(req.params.id);
+
+        if(userId){
+            await userId.update(
+              {
+                usuario: usuario,
+                email: email,
+                senha: senha
+              },
+              {
+                where: {
+                  id: req.params.id
+                }
+              }
+            );
+            res.json({msg: 'Dados do usuário alterados com sucesso'});
         }else{
-            res.status(403).json({msg: "Erro, permissão negada!"});
+          res.status(403).json({msg: "Erro, usuário inexistente!"});
         }
+      }else{
+        res.status(403).json({msg: "Erro, permissão negada!"});
+      }
     }else{
-        res.status(403).json({msg: "Erro, token inválido!"});
+      res.status(403).json({msg: "Erro, token inválido!"});
     }
-    }catch (error) {
-        res.status(400).json({mensagem: "Erro ao cadastrar administrador!", error});
-    }
+  }catch (error) {
+    res.status(400).json({mensagem: "Erro ao alterar dados, faça login.", error});
+  }
 });
 
-  module.exports = router;
+module.exports = router;
--- routes/install.js
+++ routes/install.js
@@ -8,10 +8,9 @@ const UsuarioService = require('../Model/Usuarios');
 router.get('/install', async function(req, res, next) {
     await sequelize.sync({force: true});
   
-    await UsuarioService.novo("Luiz","luiz123@gmail.com", "123456", 0);
     await UsuarioService.novo("Admin","admin@gmail.com", "123456", 1);
   
     res.json({msg: "Instalado com sucesso!"});
-  });
+});
 
-  module.exports = router;
+module.exports = router;
